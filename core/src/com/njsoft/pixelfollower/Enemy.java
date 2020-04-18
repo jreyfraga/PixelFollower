fce4bbeccf6330fb6e96ba75de43eb5f2211d52c
Jimmy King <jreyfraga@gmail.com>
2020-04-18 21:19:14 +0200

    TopBar and Sound

--- core/src/com/njsoft/pixelfollower/Enemy.java
+++ core/src/com/njsoft/pixelfollower/Enemy.java
@@ -2,7 +2,6 @@ package com.njsoft.pixelfollower;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.MathUtils;
@@ -12,14 +11,14 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import java.util.Random;
 
 
-public class Enemy extends Actor {
-    public Rectangle rectangleLogic;
-    public int movX;
-    public int movY;
+class Enemy extends Actor {
+    Rectangle rectangleLogic;
+    int movX;
+    int movY;
     private int width;
     private int height;
-    private Texture texture;
-    private Color rectangleColor;
+    private int speed;
+
     private Color[] colors;
 
     Enemy(OrthographicCamera camera) {
@@ -38,35 +37,25 @@ public class Enemy extends Actor {
         colors[4] = new Color(Color.WHITE);
         //Color aleatorio
         int rnd = new Random().nextInt(colors.length);
-        rectangleColor = colors[rnd];
-
-        movX = (int) Math.round(Math.random() * 3);
+        setColor(colors[rnd]);
+        speed= 5;
+        movX = (int) Math.round(Math.random() * speed);
         if (movX == 0) {
             rectangleLogic.setPosition(MathUtils.random(0, camera.viewportWidth), camera.viewportHeight);
-            movY = 3;
+            movY = speed;
         }
         else { //movY =0
             rectangleLogic.setPosition(camera.viewportWidth,MathUtils.random(0, camera.viewportHeight));
         }
 
-        createTexture(rectangleColor);
     }
 
-    private void createTexture(Color color) {
-        Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
-        pixmap.setColor(color);
-        pixmap.fillRectangle(0, 0, width, height);
-        texture = new Texture(pixmap);
-        pixmap.dispose();
-
-    }
-
-    public void draw(Batch batch, float parentAlpha) {
-        batch.setColor(rectangleColor.r, rectangleColor.g, rectangleColor.b, rectangleColor.a * parentAlpha);
+    void draw(Texture texture, Batch batch, float parentAlpha) {
+        batch.setColor(getColor().r, getColor().g, getColor().b, getColor().a * parentAlpha);
         batch.draw(texture, rectangleLogic.getX(), rectangleLogic.getY(), rectangleLogic.getWidth(), rectangleLogic.getHeight());
     }
 
-    public void setPosition(OrthographicCamera camera,float x, float y) {
+    void setPosition(OrthographicCamera camera, float x, float y) {
         rectangleLogic.setPosition(x,y);
         if (y < 0) { //limite pantalla
             rectangleLogic.setY(camera.viewportHeight);
--- core/src/com/njsoft/pixelfollower/PixelFollower.java
+++ core/src/com/njsoft/pixelfollower/PixelFollower.java
@@ -1,43 +1,49 @@
 package com.njsoft.pixelfollower;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
-
 
 public class PixelFollower extends Game {
 	private SpriteBatch batch;
 	private Guy goodGuy;
-	private Color color;
-	public OrthographicCamera camera;
+	private OrthographicCamera camera;
+	private int score;
     private Enemy enemy;
 	private BitmapFont font;
-	public int score;
+	private Music music;
 	private long lastDropTime;
 	private Array<Enemy> enemiesArray;
 	private long delay;
+	private Preferences prefs;
+	private Texture topBarTexture;
+	private Color topBarColor;
 
 	@Override
 	public void create () {
+		prefs = Gdx.app.getPreferences("My Preferences");
+		music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+		music.setLooping(true);
+		music.play();
 		font = new BitmapFont();
-		color =  new Color(0,0,1,1);
+		Color color = new Color(0, 0, 1, 1);
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 480,800);
-
-
 		batch = new SpriteBatch();
 		goodGuy = new Guy(camera.viewportWidth/2,20,30,30, color);
-        //enemy = new Enemy(MathUtils.random(0, camera.viewportWidth),camera.viewportHeight,10,10, new Color(1,0,0,1));
 		enemiesArray = new Array<Enemy>();
+		topBarColor = new Color(Color.PINK);
+		topBarTexture = createTexture((int) camera.viewportWidth,20,topBarColor);
 		lastDropTime = 0;
 		score = 0;
 		delay = 1000000000;
@@ -47,7 +53,6 @@ public class PixelFollower extends Game {
 	public void render () {
 
 		batch.setProjectionMatrix(camera.combined);
-
 		camera.update();
 
 		Gdx.gl.glClearColor(0, 0, 0, 1);
@@ -60,8 +65,7 @@ public class PixelFollower extends Game {
 		}
         goodGuy.draw(batch,1);
 		drawEnemies();
-        font.draw(batch,"Score: " + score, camera.viewportWidth/2-30, camera.viewportHeight-20);
-		checkScore();
+        drawTopBar();
         batch.end();
 
 	}
@@ -82,40 +86,68 @@ public class PixelFollower extends Game {
 
 		for (int iterator =0; iterator< enemiesArray.size; iterator++) {
 			enemy = enemiesArray.get(iterator);
-			enemiesArray.get(iterator).setPosition(camera, enemy.rectangleLogic.getX()-enemy.movX, enemy.rectangleLogic.getY() - enemy.movY);
-			enemy.draw(batch, 1);
+			enemiesArray.get(iterator).setPosition(camera,enemy.rectangleLogic.getX()-enemy.movX, enemy.rectangleLogic.getY() - enemy.movY);
+			enemy.draw(createTexture(10,10,enemy.getColor()),batch, 1);
 			if (enemy.rectangleLogic.overlaps(goodGuy.rectangleLogic)) {
 				enemiesArray.clear();
+				saveScore(score);
 				score=0;
+				music.stop();
+				music.play();
 			}
 		}
 
 	}
 	private void checkScore() {
 		BitmapFont message = new BitmapFont();
-		message.getData().setScale(3);
+		//message.getData().setScale(2);
 		message.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
 		if (100 <= score && score <= 110) {
-			message.draw(batch,"GREAT!!", 5,camera.viewportHeight-10);
+			message.draw(batch,"GREAT!!", 5,camera.viewportHeight-15);
 		}
 		if (200 <= score && score <= 210) {
-			message.draw(batch,"MASTER!!", 5,camera.viewportHeight-10);
+			message.draw(batch,"MASTER!!", 5,camera.viewportHeight-15);
 		}
 		if (300 <= score && score <= 310) {
-			message.draw(batch,"ULTRA!!", 5,camera.viewportHeight-10);
+			message.draw(batch,"ULTRA!!", 5,camera.viewportHeight-15);
 		}
 		if (400 <= score && score <= 410) {
-			message.draw(batch,"UNBREAKABLE!!", 5,camera.viewportHeight-10);
+			message.draw(batch,"UNBREAKABLE!!", 5,camera.viewportHeight-15);
 		}
 		if (500 <= score && score <= 510) {
-			message.draw(batch,"GOD!!", 5,camera.viewportHeight-10);
+			message.draw(batch,"GOD!!", 5,camera.viewportHeight-15);
 		}
 		if (600 <= score && score <= 610) {
-			message.draw(batch,"STOP THIS!!", 5,camera.viewportHeight-10);
+			message.draw(batch,"STOP THIS!!", 5,camera.viewportHeight-15);
 		}
 		if (700 <= score && score <= 710) {
-			message.draw(batch,"MADNESS!!", 5,camera.viewportHeight-10);
+			message.draw(batch,"MADNESS!!", 5,camera.viewportHeight-15);
 		}
+	}
+	private void saveScore(int score) {
+		if (score > prefs.getInteger("score", 0)) {
+			prefs.putInteger("score", score);
+			prefs.flush();
+		}
+
+	}
+
+	private void drawTopBar() {
+		batch.setColor(topBarColor);
+		batch.draw(topBarTexture, 0, camera.viewportHeight-40, camera.viewportWidth, 40);
+		font.draw(batch,"Score: " + score, camera.viewportWidth/2-30, camera.viewportHeight-15);
+		font.draw(batch,"Record: " + prefs.getInteger("score", 0), camera.viewportWidth*3/4, camera.viewportHeight-15);
+		checkScore();
+	}
+
+	private Texture createTexture(int width, int height,Color color) {
+		Texture texture;
+		Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+		pixmap.setColor(color);
+		pixmap.fillRectangle(0, 0, width, height);
+		texture = new Texture(pixmap);
+		pixmap.dispose();
+		return texture;
 	}
 }
