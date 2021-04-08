fb298bf8c60fba1ef433eb5e7afa416b67ce8c48
JimmyKingF <jreyfraga@gmail.com>
2021-04-08 16:22:21 +0200

    Update Guy.java
    Cambiado en Github

--- core/src/com/njsoft/pixelfollower/Guy.java
+++ core/src/com/njsoft/pixelfollower/Guy.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-#NJ2
+#NJ Git
 
 public class Guy extends Actor {
     public Rectangle rectangleLogic;
